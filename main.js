fd9753acfbdc1a65b53a87e0c3059a57260b6756
Nahid fatima <fatimanahid04@gmail.com>
2020-02-27 12:18:40 +0530

    #Nahid Fatima:Defining Default Parameter

---
+++ main.js
@@ -0,0 +1,15 @@
+function onDisplay(strFirstName,strLastName,numAge=0,address=undefined){
+    console.log(strFirstName);
+    console.log(strLastName);
+    console.log(numAge);
+    //Exception will Throw
+    //console.log(address);
+}
+
+//onDisplay("Nahid","Ansari",28);
+
+//onDisplay("Nahid","Ansari");
+
+//onDisplay("Nahid");
+
+onDisplay("Nahid","Ansari",28,"Thane");
